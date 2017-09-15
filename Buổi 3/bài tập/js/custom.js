9885fc90ce6f5e621acea6ca7f517a923e5715a1
LdDv9 <ledungb11@gmail.com>
2017-09-15 08:55:19 +0700

    dun add all file

---
+++ Buổi 3/bài tập/js/custom.js
@@ -0,0 +1,10 @@
+function myclick() {
+    // body... 
+     document.getElementById("tab-feature7").style.display = "block";
+      document.getElementById("btn-off").style.display = "none";
+}
+function clickoff() {
+    // body... 
+     document.getElementById("tab-feature7").style.display = "none";
+      document.getElementById("btn-off").style.display = "block";
+}
---
+++ Buổi 3/bài tập/js/dangkyhoc.js
@@ -0,0 +1,10 @@
+function myclick() {
+    // body... 
+     document.getElementById("tab-feature2").style.display = "block";
+      document.getElementById("btn-off").style.display = "none";
+}
+function clickoff() {
+    // body... 
+     document.getElementById("tab-feature2").style.display = "none";
+      document.getElementById("btn-off").style.display = "block";
+}
